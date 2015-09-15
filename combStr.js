318e7ebd4602de75edaac36def36a81ed844faaa
Intekhab Alam <intekhab.alam@paytm.com>
2015-09-15 11:08:09 +0530

    changes in combStr

--- combStr.js
+++ combStr.js
@@ -1,13 +1,21 @@
-var originalStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-var originalStr = 'MAISNKS';
-var length = 5;
-subStr('', originalStr, length);
-function subStr(str, remain, length) {
+/*
+Program to print all possible substring of a string of arbitrary length
+*/
 
-	if ((str+remain).length < length) {
-		console.log('here');
+
+//var originalStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var originalStr = 'MAISNKS';// This is the original string
+var length = 5;// This is the arbitrary length of what you want to make substring
+main(originalStr, length);// call main function by passing original string and arbitray length
+function main(str, length) {
+	if (str.length < length) {// show error and return false if string length is less than arbitray length
+		console.log('Provided string is less than the length of what you want to make substring');
 		return false;
 	}
+
+	subStr('', originalStr, length);
+}
+function subStr(str, remain, length) {
 
 	if (str.length == length) {
 		console.log(str);
@@ -17,7 +25,6 @@ function subStr(str, remain, length) {
 	for (var i = 0; i < remain.length; i++) {
 		var st =  remain.charAt(i);
 		var rem = remain.substring(0, i) + remain.substring(i+1);
-		//console.log(st);
 		subStr(st + str, rem, length);
 	}
 }
